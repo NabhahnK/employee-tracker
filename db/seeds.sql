8f1050f3a219f0d71b1600adbb6242f0df91ec07
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-07-24 12:42:05 -0700

    made seeds for testing

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+USE employees_db;
+
+INSERT INTO department (id, name)
+VALUES
+    (5, The Department Of Stuff!);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES
+    (3, Sammy, 45.00, 5),
+    (7, Dammy, 45.00, 5);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES 
+    (4, The Sammy, Lost Sammy, 3),
+    (8, The Dammy, Lost Dammay, 7, 4);
