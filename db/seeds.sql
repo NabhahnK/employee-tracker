4f8c577600fae59c09ce13c6ad7aeae77bb4dd1f
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-07-24 12:57:14 -0700

    finished seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,14 +2,14 @@ USE employees_db;
 
 INSERT INTO department (id, name)
 VALUES
-    (5, The Department Of Stuff!);
+    (5, "The Department Of Stuffs");
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES
-    (3, Sammy, 45.00, 5),
-    (7, Dammy, 45.00, 5);
+    (3, "Sammy", 45.00, 5),
+    (7, "Dammy", 45.00, 5);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES 
-    (4, The Sammy, Lost Sammy, 3),
-    (8, The Dammy, Lost Dammay, 7, 4);
+    (4, "The Sammy", "Lost Sammy", 3, NULL),
+    (8, "The Dammy", "Lost Dammay", 7, 4);
