8eacad2101776e97a3c3d694a65e45bfe8fa00d8
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-07-24 12:32:47 -0700

    finished schema

--- db/schema.sql
+++ db/schema.sql
@@ -12,5 +12,21 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary 
-)
+    salary DECIMAL,
+    department_id,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id,
+    CONSTRAINT
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
+);
