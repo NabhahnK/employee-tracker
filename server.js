18ab2837e2c30a6a8fb6d2e7c530cb6ff797e9d9
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-07-26 15:25:54 -0700

    added a question and rolled back an inquirer version

--- server.js
+++ server.js
@@ -7,36 +7,22 @@ function init() {
 }
 
 function mainOptions() {
-    // First question is askd
-    // inquirer
-    //     .prompt([
-    //         {
-    //             name: "managerName",
-    //             type: "input",
-    //             message: "What is your managers name?",
-    //         },
-    //         {
-    //             name: "managerID",
-    //             type: "number",
-    //             message: "What is their ID number?"
-    //         },
-    //         {
-    //             name: "managerEmail",
-    //             type: "input",
-    //             message: "What is their Email?"
-    //         },
-    //         {
-    //             name: "managerOfficeNumber",
-    //             type: "number",
-    //             message: "What is their office number?"
-    //         }
-    //     ])
-    //     .then((answers) => {
-    //         const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber);
-    //         team.push(manager);
-    //         console.log(team);
-    //         checkBuildStatus();
-    //     });
+    // First question is asked
+    inquirer
+        .prompt([
+            {
+                name: "option",
+                type: "list",
+                message: "What do you want to do?",
+                choices: [ "View all Departments", "choice B" ]
+            }
+        ])
+        .then((answer) => {
+            const option = answer;
+            // team.push(manager);
+            console.log(option);
+            // checkBuildStatus();
+        });
 }
 
 init();
