6bbe22151415cc73782ca04f029c225fb23a297c
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-07-26 15:54:09 -0700

    added mysql connection, then commeted it bout

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const mysql = require("mysql2");
 const cTable = require("console.table");
 const inquirer = require("inquirer");
 
+// Connect to database
+// const db = mysql.createConnection(
+//     {
+//         host: process.env.DB_LOCATION,
+//         // MySQL username,
+//         user: process.env.DB_USER,
+//         // MySQL password
+//         password: process.env.DB_PASSWORD,
+//         database: 'employees_db'
+//     },
+//     console.log(`Connected to the employees_db database.`)
+// );
+
 function init() {
     mainOptions();
 }
@@ -14,8 +27,8 @@ function mainOptions() {
                 name: "option",
                 type: "list",
                 message: "What do you want to do?",
-                choices: [ "View all Departments", "View all Roles", "View all Employees",
-                        "Add a Department", "Add a Role", "Add an Employee"]
+                choices: ["View all Departments", "View all Roles", "View all Employees",
+                    "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"]
             }
         ])
         .then((answer) => {
